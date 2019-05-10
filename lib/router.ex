d85f62684c4be8a577075fee9f6d1d4f1c167513
MarlonLop <2923927+MarlonLop@users.noreply.github.com>
2019-05-09 18:36:26 -0600

    it works! serves html file only

--- lib/router.ex
+++ lib/router.ex
@@ -1,12 +1,47 @@
+
 defmodule HttpServer.Router do
+  @moduledoc """
+  This module handle all routin for the Server
+  when sending a curl command there is no need to add '.html' extension
+  example curl localhost:8080/hello
+  """
+  require Logger
   use Plug.Router
   plug :match     #to match routes
-  plug :dispatch
+  plug :dispatch  #receive
+  plug Plug.Static, at: "/home", from: {:http_server, "priv/static"}
 
+  # function to root index.html
   get "/" do
-    send_resp(conn, 200, "Hello World!")
-  end
+      conn
+      |> put_resp_header("content-type", "text/html; charset=utf-8")
+      |> send_file(:ok, "priv/static/index.html")
+    end
 
-  match _, do:
-    send_resp(conn, 404, "404 Page not found")
-  end
+    # default function to handle any route that is not root '/'
+    get "/:match_page" do
+      cond do
+          File.exists?("priv/static/#{match_page}.html") ->
+            conn
+            #|> Plug.Conn.register_before_send(Logger.info("Sent a #{conn.status} response"))
+            |> put_resp_header("content-type", "text/html; charset=utf-8")
+            |> send_file(:ok, "priv/static/#{match_page}.html")
+
+          true ->
+            conn
+            |> put_resp_header("content-type", "text/html; charset=utf-8")
+            |> send_file(:ok, "priv/static/404.html")
+
+      end
+
+      end
+end
+#Logger.info "HTTP/1.1 200 OK"
+#send_resp(conn, 200, "HTTP/1.1 200 OK")
+
+  # match _, do:
+  #   send_resp(conn, 404, "404 Page not found")
+  # end
+
+# GET hello HTTP/1.1
+# host: localhost
--- mix.exs
+++ mix.exs
@@ -22,13 +22,10 @@ defmodule HttpServer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      #package to handle routes
-      {:plug, "~> 1.8"},
-      #package for
+      #package for server
       {:plug_cowboy, "~> 1.0"},
-      #{:cowboy, "~> 2.6"}
+      #package to handle routes
+      {:plug, "~> 1.0"}
     ]
   end
 end
